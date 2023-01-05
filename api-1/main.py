1821f2301739bef9130c0c26af7230efd4139e29
Braian <bryansalles45@gmail.com>
2023-01-04 23:32:31 -0300

    query param

--- api-1/main.py
+++ api-1/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {"message": "Hello"}
 
+#query param
+@app.get('/dobro')
+async def quad(num: int):
+    text = f'o dobro de {num} é {num**2}'
+    return {"msg": text}
 
